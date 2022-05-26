a176b5c81459a44d85d26ed44f11981dae27fcca
Rijo do q'até t'apagas <guilherme.belchior1@hotmail.com>
2022-05-26 19:39:22 +0100

    Separated the book and subs array

--- books.js
+++ books.js
@@ -1,18 +1,23 @@
 const books = [
-        {Subject: 'technologies', Author: "Robert C. Martin", Title: 'Clean Code: A Handbook of Agile Software Craftsmanship'},
-        {Subject: 'technologies', Author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein", Title: 'Introduction to Algorithms'},
-        {Subject: 'technologies', Author: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", Title: 'Structure and Interpretation of Computer Programs (SICP)'},
-        {Subject: 'technologies', Author: "Robert C. Martin", Title: 'The Clean Coder: A Code of Conduct for Professional Programmers'},
-        {Subject: 'technologies', Author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides, Grady Booch", Title: 'Design Patterns: Elements of Reusable Object-Oriented Software'},
-        {Subject: 'Physics', Author: "Karl F. Kuhn, Frank Noschese", Title: 'Basic Physics: A Self-Teaching Guide'},
-        {Subject: 'Biology', Author: "Johnjoe McFadden", Title: 'Quantum Evolution: Life in the Multiverse'},
-        {Subject: 'Chemistry', Author: "César Vega, Job Ubbink, Erik van der Linden, Jeffrey Steingarten", Title: 'The Kitchen as Laboratory: Reflections on the Science of Food and Cooking'},
-        {Subject: 'Physics', Author: "Manjit Kumar", Title: 'Quantum Einstein debate'},
-        {Subject: 'Biology', Author: "Jim Al-Khalili", Title: 'Life on the Edge: The Coming of Age of Quantum Biology'},
-        {Subject: 'Physics', Author: "Alistair I. M. Rae", Title: 'Quantum physics: A beginner’s guide'},
-        {Subject: 'Biography', Author: "Cristina Ferreira", Title: 'The beloved Supreme God Belchior'},
-        {Subject: 'Chemistry', Author: "Adam Sharples, James Morton, Henning Wackerhage", Title: 'Molecular Exercise Physiology'},
-        {Subject: 'Physics', Author: "Dr. Michio Kaku", Title: 'The God Equation: The Quest for a Theory of Everything'},
-        {Subject: 'Biography', Author: "Andrew Hodges", Title: 'Alan Turing: The Enigma'},
-        {Subject: 'Economics', Author: "Thomas Sowell", Title: 'Basic Economics'}
-]
+        {Subject: 'technologies', Author: "Robert C. Martin", Title: 'Clean Code: A Handbook of Agile Software Craftsmanship', id: 1},
+        {Subject: 'technologies', Author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein", Title: 'Introduction to Algorithms', id: 2},
+        {Subject: 'technologies', Author: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", Title: 'Structure and Interpretation of Computer Programs (SICP)', id: 3},
+        {Subject: 'technologies', Author: "Robert C. Martin", Title: 'The Clean Coder: A Code of Conduct for Professional Programmers', id: 4},
+        {Subject: 'technologies', Author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides, Grady Booch", Title: 'Design Patterns: Elements of Reusable Object-Oriented Software', id: 16},
+        {Subject: 'Physics', Author: "Karl F. Kuhn, Frank Noschese", Title: 'Basic Physics: A Self-Teaching Guide', id: 5},
+        {Subject: 'Biology', Author: "Johnjoe McFadden", Title: 'Quantum Evolution: Life in the Multiverse', id: 6},
+        {Subject: 'Chemistry', Author: "César Vega, Job Ubbink, Erik van der Linden, Jeffrey Steingarten", Title: 'The Kitchen as Laboratory: Reflections on the Science of Food and Cooking', id: 7},
+        {Subject: 'Physics', Author: "Manjit Kumar", Title: 'Quantum Einstein debate', id: 8},
+        {Subject: 'Biology', Author: "Jim Al-Khalili", Title: 'Life on the Edge: The Coming of Age of Quantum Biology', id: 9},
+        {Subject: 'Physics', Author: "Alistair I. M. Rae", Title: 'Quantum physics: A beginner’s guide', id: 10},
+        {Subject: 'Biography', Author: "Cristina Ferreira", Title: 'The beloved Supreme God Belchior', id: 11},
+        {Subject: 'Chemistry', Author: "Adam Sharples, James Morton, Henning Wackerhage", Title: 'Molecular Exercise Physiology', id: 12},
+        {Subject: 'Physics', Author: "Dr. Michio Kaku", Title: 'The God Equation: The Quest for a Theory of Everything', id: 13},
+        {Subject: 'Biography', Author: "Andrew Hodges", Title: 'Alan Turing: The Enigma', id: 14},
+        {Subject: 'Economics', Author: "Thomas Sowell", Title: 'Basic Economics', id: 15}
+    ]
+    
+    const aux = books.map(book => {return book.Subject});
+    const subs = aux.filter((item,pos) => {return aux.indexOf(item) == pos});
+
+module.exports = {books, subs};
--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const Joi = require('joi'); //Joi 13.1.0
 const express = require('express');
-const { workerData } = require('worker_threads');
+const {books, subs} = require('./books.js');
 const app = express();
 
-const books = [
-    {Subject: 'technologies', Author: "Robert C. Martin", Title: 'Clean Code: A Handbook of Agile Software Craftsmanship'},
-    {Subject: 'technologies', Author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein", Title: 'Introduction to Algorithms'},
-    {Subject: 'technologies', Author: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", Title: 'Structure and Interpretation of Computer Programs (SICP)'},
-    {Subject: 'technologies', Author: "Robert C. Martin", Title: 'The Clean Coder: A Code of Conduct for Professional Programmers'},
-    {Subject: 'technologies', Author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides, Grady Booch", Title: 'Design Patterns: Elements of Reusable Object-Oriented Software'},
-    {Subject: 'Physics', Author: "Karl F. Kuhn, Frank Noschese", Title: 'Basic Physics: A Self-Teaching Guide'},
-    {Subject: 'Biology', Author: "Johnjoe McFadden", Title: 'Quantum Evolution: Life in the Multiverse'},
-    {Subject: 'Chemistry', Author: "César Vega, Job Ubbink, Erik van der Linden, Jeffrey Steingarten", Title: 'The Kitchen as Laboratory: Reflections on the Science of Food and Cooking'},
-    {Subject: 'Physics', Author: "Manjit Kumar", Title: 'Quantum Einstein debate'},
-    {Subject: 'Biology', Author: "Jim Al-Khalili", Title: 'Life on the Edge: The Coming of Age of Quantum Biology'},
-    {Subject: 'Physics', Author: "Alistair I. M. Rae", Title: 'Quantum physics: A beginner’s guide'},
-    {Subject: 'Biography', Author: "Cristina Ferreira", Title: 'The beloved Supreme God Belchior'},
-    {Subject: 'Chemistry', Author: "Adam Sharples, James Morton, Henning Wackerhage", Title: 'Molecular Exercise Physiology'},
-    {Subject: 'Physics', Author: "Dr. Michio Kaku", Title: 'The God Equation: The Quest for a Theory of Everything'},
-    {Subject: 'Biography', Author: "Andrew Hodges", Title: 'Alan Turing: The Enigma'},
-    {Subject: 'Economics', Author: "Thomas Sowell", Title: 'Basic Economics'}
-]
-
-const aux = books.map(book => {return book.Subject});
-const subs = aux.filter((item,pos) => {return aux.indexOf(item) == pos});
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -31,37 +10,75 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/subjects', (req, res) => {
+    //sends all the subjects on the server
     res.send(subs);
 })
 
 
 app.get('/api/subjects/:sub', (req, res) => {
-    const list = (books.filter(book => book.Subject.toLowerCase().trim().includes(req.params.sub.toLowerCase().trim())));
-    if (list == []) return res.status(404).send('Subject not found');
-    res.send(list);
 
+    //filters the books to just include the ones with the specified subject
+    const list = (books.filter(book => book.Subject.toLowerCase().trim().includes(req.params.sub.toLowerCase().trim())));
+    //If the list is empty, there's no book with such subject -> error 404, exits
+    if (list == []) return res.status(404).send('Subject not found');
+
+    //sends all books with the specified subject
+    res.send(list);
 })
 
+//Function that checks if there's any book with the specified subject
+const inSubsList = () => subs.find(sub => sub.Subject.toLowerCase().trim().includes(subject.toLowerCase().trim()));
+
 app.post('/api', (req, res) =>    {
+    //Adds a new book to the api
     const {subject, title, author} = req.body;
+
+    //Checks if there's a book with the same title
     const found = books.find(book => book.Title.toLowerCase() == title.toLowerCase())
-    
     if (found) return res.status(400).send('Book already exists');
     
-    books.push({Subject: subject, Title: title, Author: author});
 
-    const inSubsList = () => subs.find(sub => sub.Subject.toLowerCase().trim().includes(subject.toLowerCase().trim()));
-
+    books.push({Subject: subject, Title: title, Author: author, id: books.length + 1 });
+    //If the subject on the subs array, adds it.
     if (!inSubsList) subs.push(subject);
 
     res.status(201);
 })
 
-// app.put()
+app.put('/api/subjects/:id', (req, res) => {
+    //Changes a book on the API
+    const {title, author, subject} = req.body;
 
-// app.delete()
+    //Finds the book with the specified id
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('Theres\'s no book with id ' + req.params.id);
+    
+    //Changes the book
+    book.Subject =  subject;
+    book.Author = author;
+    book.Title=  title;
 
-const port = process.env.PORT || 3000;
+    //If the new subject is not on the subs array, adds it.
+    if (!inSubsList) subs.push(subject);
+
+    res.status(201).send(book);
+});
+
+app.delete('/api/subjects/:id', (req, res) => {
+    //Removes a book from the API
+
+    //Finds the book with the specified id
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('Theres\'s no book with id ' + req.params.id);
+
+    //Gets the book index and removes it.
+    const index = books.indexOf(book);
+    books.splice(index, 1);
+    res.status(201).send(book)
+}) 
+
+//Tries to get the port, if not available, uses default port 3000
+const port = process.env.PORT || 3000; 
 
 app.listen(port, (req, res) => {
     console.log(`listening on port ${port}...`);
